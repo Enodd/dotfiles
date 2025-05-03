47492f342aae13a00d4762e67747e82227a02ceb
enodd <foxnacity@gmail.com>
2025-05-03 09:03:45 +0200

    feat: add nvm loading for linux and modify gitignore

--- .config/zsh/scripts.sh
+++ .config/zsh/scripts.sh
@@ -17,11 +17,6 @@ update() {
 
 alias dev="cd $HOME/Development/"
 
-export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-
 autoload -U add-zsh-hook
 load-nvmrc() {
   local nvmrc_path="$(nvm_find_nvmrc)"
--- .zshrc
+++ .zshrc
@@ -4,3 +4,7 @@ export PHP_INI_SCAN_DIR="$HOME/.config/herd-lite/bin:$PHP_INI_SCAN_DIR"
 export PATH="${PATH}:${HOME}/Library/Python/3.9/bin"
 
 alias lg="lazygit"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
