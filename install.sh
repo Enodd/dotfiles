6f5f0b4d84f129e5b3cacf6bce8565514143a9c3
enodd <foxnacity@gmail.com>
2025-05-03 09:55:36 +0200

    feat: add font installation

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Setup config for archlinux for date: $(date)"
 
 echo "Installing required packages"
-sudo pacman -Syu zsh fastfetch fakeroot make gcc tar waybar hyprland alacritty stow nvm go neovim hyprpaper hyprlock
+sudo pacman -Syu zsh fastfetch fakeroot make gcc tar waybar hyprland alacritty stow nvm go neovim hyprpaper hyprlock $(pacman -Ssq ttf-)
 
 echo "Installing rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
